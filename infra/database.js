50a8265744df7089c213002285f89379799a8c3c
Mateus Santana <mateus.attractor779@passinbox.com>
2025-04-09 20:38:32 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,15 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log('Credenciais do Postgres',{
+  console.log("Credenciais do Postgres", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  } )
+  });
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -23,7 +24,7 @@ async function query(queryObject) {
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    // await client.end();
   }
 }
 export default {
